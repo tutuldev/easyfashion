5021929c3aadaf0042d3d077fe74e050bc7b73dd
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-06-04 11:02:55 +0600

    jins section design update

--- resources/views/frontend/layouts/jins.blade.php
+++ resources/views/frontend/layouts/jins.blade.php
@@ -1,7 +1,15 @@
 <section class="trending bg-white px-4 py-8">
-  <div class="max-w-screen-xl mx-auto">
-    <a href="#" class="block"> 
-      <img src="{{ asset('images/jins.png') }}" alt="Jins image">
-    </a>
-  </div>
+    <div class="max-w-screen-xl mx-auto relative">
+        <a href="#" class="block cursor-pointer"> <img src="{{ asset('images/jins.webp') }}" alt="Jeans image" class="w-full h-auto object-cover rounded-lg">
+
+            <div class="absolute inset-0 flex flex-col items-center justify-center text-green-600 p-4">
+                <h2 class="text-2xl md:text-4xl lg:text-7xl font-extrabold tracking-wide mb-2">
+                    JEANS
+                </h2>
+                <p class="text-sm md:text-xl lg:text-2xl font-bold">
+                    STARTING AT ট 2,490
+                </p>
+            </div>
+        </a>
+    </div>
 </section>
